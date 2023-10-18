49907f5071b1f8c182839f976a9409173ea178c4
Inseminador <146298402+Inseminador@users.noreply.github.com>
2023-10-18 19:53:49 -0300

    Update ATV-1.java

--- ATV-1.java
+++ ATV-1.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    //Criar um algoritmo que imprima a sequência dos 100 primeiros números pares
+    //Criar um algoritmo que imprima a sequência dos 100 primeiros números pares.
     int mult = 1;
     for(int num = 2;(num*mult)<=100; mult++){
     System.out.println(num*mult);
